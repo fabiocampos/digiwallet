f8a316cc5f1c113f81673dd5d9c53af93aae45ce
Fabio Campos <fabiocamposrj@gmail.com>
2018-08-04 12:06:46 -0300

    Initial Commit

---
+++ Digiwallet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Digiwallet
+//
+//  Created by Fabio Campos on 04/08/2018.
+//  Copyright © 2018 green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Digiwallet/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Digiwallet
+//
+//  Created by Fabio Campos on 04/08/2018.
+//  Copyright © 2018 green. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
