bd7e93d3cdd1aa45249f75c7b07a38f8d15858ef
Fabio Campos <fabiocamposrj@gmail.com>
2018-08-14 03:09:47 -0300

    Atualização da api Brita para evitar valores vazios durante o primeiro dia da semana

--- Digiwallet/Api/BritaApi.m
+++ Digiwallet/Api/BritaApi.m
@@ -14,7 +14,7 @@
 
 @implementation BritaApi
 NSString *const BRAZILIAN_CENTRAL_BANK_API = @"https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/";
-NSString *const DAILY_COTATION_ENDPOINT = @"CotacaoDolarDia(dataCotacao=@dataCotacao)?%@%@%@";
+NSString *const DAILY_COTATION_ENDPOINT = @"CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?%@%@%@%@";
 
 - (instancetype)init{
     self = [super init];
@@ -32,13 +32,14 @@ NSString *const DAILY_COTATION_ENDPOINT = @"CotacaoDolarDia(dataCotacao=@dataCot
     NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
     [dateFormat setDateFormat:@"MM-dd-yyyy"];
     NSString *cotationDateFormatted = [dateFormat stringFromDate:cotationDate];
-   NSString *enpoint = [NSString stringWithFormat:DAILY_COTATION_ENDPOINT,@"%40dataCotacao=%27", cotationDateFormatted, @"%27&%24format=json"];
+   NSString *enpoint = [NSString stringWithFormat:DAILY_COTATION_ENDPOINT,@"%40moeda=%27USD%27", @"&%40dataCotacao=%27", cotationDateFormatted, @"%27&%24format=json"];
     //@Todo create a failover for holydays
     [self.networkingManager GET:enpoint parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         if(responseObject != nil && [[responseObject objectForKey:@"value"] count] > 0){
             CoinPrice *britaPrice = [[CoinPrice alloc] init];
-            britaPrice.buyValue = responseObject[@"value"][0][@"cotacaoCompra"];
-            britaPrice.sellValue = responseObject[@"value"][0][@"cotacaoVenda"];
+            NSInteger latestIndext = [responseObject[@"value"] count] - 1;
+            britaPrice.buyValue = responseObject[@"value"][latestIndext][@"cotacaoCompra"];
+            britaPrice.sellValue = responseObject[@"value"][latestIndext][@"cotacaoVenda"];
             britaPrice.type = kBrita;
             britaPrice.name = @"Brita";
             success(britaPrice);
@@ -61,10 +62,12 @@ NSString *const DAILY_COTATION_ENDPOINT = @"CotacaoDolarDia(dataCotacao=@dataCot
     NSCalendar *calendar = [NSCalendar currentCalendar];
     NSInteger weekday = [calendar component:NSCalendarUnitWeekday
                                    fromDate:cotationDate];
+    NSDateComponents *components = [calendar components:(NSCalendarUnitHour | NSCalendarUnitMinute) fromDate:cotationDate];
+    NSInteger hour = [components hour];
     if(weekday == 1){
         [dateComponents setDay:-2];
         cotationDate = [[NSCalendar currentCalendar] dateByAddingComponents:dateComponents toDate:cotationDate options:0];
-    }else if(weekday == 7){
+    }else if(weekday == 7 || (hour >=0 && hour <= 10)){
         [dateComponents setDay:-1];
         cotationDate = [[NSCalendar currentCalendar] dateByAddingComponents:dateComponents toDate:cotationDate options:0];
     }
